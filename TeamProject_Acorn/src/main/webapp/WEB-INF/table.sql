6e08b2f32341b3a0f5b24eca928faaf04dfa590a
leesangmyeong <leesangmyeong230@gmail.com>
2020-02-26 14:55:56 +0900

    게시판 테이블 / 댓글 테이블 추가

--- TeamProject_Acorn/src/main/webapp/WEB-INF/table.sql
+++ TeamProject_Acorn/src/main/webapp/WEB-INF/table.sql
@@ -9,7 +9,6 @@ likeCount NUMBER, --추천수
 regdate DATE
 );
 
-
 CREATE SEQUENCE aboard_cafe_seq;
 
 --게시판 댓글 정보 테이블--
@@ -23,5 +22,5 @@ comment_group NUMBER, --원글에 달린 댓글 내에서의 그룹번호
 deleted CHAR(3) DEFAULT 'no', --댓글이 삭제되었는지 여부
 regdate DATE --댓글 등록일
 );
-+CREATE SEQUENCE aboard_cafe_comment_seq;
 
+CREATE SEQUENCE aboard_cafe_comment_seq;
