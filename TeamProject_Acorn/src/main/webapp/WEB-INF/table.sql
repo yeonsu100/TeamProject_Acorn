67f5439cda551170fbc416ddef4b6bb55ae72296
leesangmyeong <leesangmyeong230@gmail.com>
2020-02-26 19:04:28 +0900

    작업을 수행할 테이블 생성

--- TeamProject_Acorn/src/main/webapp/WEB-INF/table.sql
+++ TeamProject_Acorn/src/main/webapp/WEB-INF/table.sql
@@ -52,3 +52,30 @@ regdate DATE 	                   -- 작성 날짜
 
 CREATE SEQUENCE T_notice_seq;
 
+--게시판 table 
+CREATE TABLE T_board( 
+num NUMBER PRIMARY KEY,
+writer VARCHAR2(100) NOT NULL,
+title VARCHAR2(100) NOT NULL,
+content CLOB,
+viewCount NUMBER,
+likeCount NUMBER, --추천수
+regdate DATE
+);
+
+ CREATE SEQUENCE T_board_seq;
+ 
+ --게시판 댓글 정보 테이블
+ CREATE TABLE T_board_comment(
+num NUMBER PRIMARY KEY, -- 댓글의 글번호
+writer VARCHAR2(100),
+content VARCHAR2(500),
+target_id VARCHAR2(100), -- 댓글의 대상이 되는 아이디 (글작성자)
+ref_group NUMBER, --댓글의 그룹번호
+comment_group NUMBER, --원글의 달린 댓글 내에서의 그룹번호
+deleted CHAR(3) DEFAULT 'no', -- 댓글이 삭제 되었는지 여부
+profile VARCHAR2(100), -- 프로필
+regdate DATE -- 댓글 등록일
+ );
+ 
+ CREATE SEQUENCE T_board_comment_seq;
