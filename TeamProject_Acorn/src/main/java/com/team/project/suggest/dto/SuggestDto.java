9246487b77ad9d4b697e261ac94d6a234bfeee1d
hanseokho <gkstjrgh4@naver.com>
2020-03-10 18:32:39 +0900

    진행중

--- TeamProject_Acorn/src/main/java/com/team/project/suggest/dto/SuggestDto.java
+++ TeamProject_Acorn/src/main/java/com/team/project/suggest/dto/SuggestDto.java
@@ -8,11 +8,14 @@ public class SuggestDto {
 	private String regdate;	  // 건의 날짜
 	private int startRowNum;  // 페이징 처리용 시작번호
 	private int endRowNum;    // 페이징 처리용 끝 번호
+	private String sugProfile; // 작성자의 이미지를 담을 필드
 	// 디폴트 생성자
 	public SuggestDto() {}
 	
-	public SuggestDto(int num, String sugId, String sugContent, String regdate, int startRowNum,
-			int endRowNum) {
+	
+	
+	public SuggestDto(int num, String sugId, String sugContent, String regdate, int startRowNum, int endRowNum,
+			String sugProfile) {
 		super();
 		this.num = num;
 		this.sugId = sugId;
@@ -20,58 +23,53 @@ public class SuggestDto {
 		this.regdate = regdate;
 		this.startRowNum = startRowNum;
 		this.endRowNum = endRowNum;
+		this.sugProfile = sugProfile;
 	}
+
+
 
 	public int getNum() {
 		return num;
 	}
-
 	public void setNum(int num) {
 		this.num = num;
 	}
-
 	public String getSugId() {
 		return sugId;
 	}
-
 	public void setSugId(String sugId) {
 		this.sugId = sugId;
 	}
-
-
 	public String getSugContent() {
 		return sugContent;
 	}
-
 	public void setSugContent(String sugContent) {
 		this.sugContent = sugContent;
 	}
-
 	public String getRegdate() {
 		return regdate;
 	}
-
 	public void setRegdate(String regdate) {
 		this.regdate = regdate;
 	}
-
 	public int getStartRowNum() {
 		return startRowNum;
 	}
-
 	public void setStartRowNum(int startRowNum) {
 		this.startRowNum = startRowNum;
 	}
-
 	public int getEndRowNum() {
 		return endRowNum;
 	}
-
 	public void setEndRowNum(int endRowNum) {
 		this.endRowNum = endRowNum;
 	}
-	
-
+	public String getSugProfile() {
+		return sugProfile;
+	}
+	public void setSugProfile(String sugProfile) {
+		this.sugProfile = sugProfile;
+	}
 	
 	
 }
