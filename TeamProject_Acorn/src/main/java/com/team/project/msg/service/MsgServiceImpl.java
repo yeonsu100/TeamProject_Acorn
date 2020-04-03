7c901580019e0e3983da023ffa9100334a02f784
leechangho <maltitol7090@gmail.com>
2020-04-03 12:01:08 +0900

    메시지함 핑크핑크

--- TeamProject_Acorn/src/main/java/com/team/project/msg/service/MsgServiceImpl.java
+++ TeamProject_Acorn/src/main/java/com/team/project/msg/service/MsgServiceImpl.java
@@ -20,9 +20,9 @@ public class MsgServiceImpl implements MsgService{
 	private MsgDao dao;
 	
 	//페이지당 글 개수
-	final int PAGE_ROW_COUNT=5;
+	final int PAGE_ROW_COUNT=7;
 	//한번에 보여지는 페이지개수
-	final int PAGE_DISPLAY_COUNT=5;
+	final int PAGE_DISPLAY_COUNT=3;
 
 	@Override
 	public void list(HttpServletRequest request) {
